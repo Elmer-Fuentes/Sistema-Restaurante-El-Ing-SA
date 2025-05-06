ec92c5d86b1e2af08d8e85de533c7301779cf2f0
Fuentes-Elmer <delacruzelmer27@gmail.com>
2025-05-06 00:01:29 -0600

    Instancia clase Conexión en clase C_seguridad

--- Datos/cd_Login.cs
+++ Datos/cd_Login.cs
@@ -15,8 +15,23 @@ namespace capa_datos
         private conexion connex = new conexion();
         #endregion
 
+        #region ="MEtodo Login"
+        public DataTable MtdLogin(string usuario, string contrasena)
+        {
+            string query = "SELECT nombre_usuario, rol FROM tbl_usuarios WHERE nombre_usuario COLLATE Latin1_General_CS_AS = @usuario AND contrasena COLLATE Latin1_General_CS_AS = @contrasena";
+            DataTable resultado = new DataTable();
+            using (SqlConnection conexion = connex.MtdAbrirconexion())
+            using (SqlCommand cmd = new SqlCommand(query, conexion))
+            {
+                cmd.Parameters.AddWithValue("@usuario", usuario);
+                cmd.Parameters.AddWithValue("@contrasena", contrasena);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(resultado);
+            }
+            return resultado;
+        }
+        #endregion
 
-       
 
 
     }
---
+++ Seguridad/C_seguridad.cs
@@ -0,0 +1,18 @@
+﻿using capa_datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seguridad
+{
+    public class C_seguridad
+    {
+        #region = "instancias de la clase conexion";
+        private conexion connex = new conexion();
+        private cd_Usuarios cd_user_login = new cd_Usuarios();
+        #endregion
+
+    }
+}
