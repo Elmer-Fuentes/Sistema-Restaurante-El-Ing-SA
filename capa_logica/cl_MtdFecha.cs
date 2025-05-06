1684ff09c6b425370245b213cb00c5b9e94815ed
Dcruz11 <davidestuardo0802@gmail.com>
2025-05-05 23:39:47 -0600

    creacion de la clase cd_clientes

---
+++ capa_logica/cl_MtdFecha.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capa_logica
+{
+    public class cl_MtdFecha
+    {
+        public DateTime MtdFecha()
+        {
+            return DateTime.Now;
+        }
+    }
+}
