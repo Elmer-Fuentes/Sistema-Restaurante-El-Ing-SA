9f6b20fd39de18b7ded5d882525e08ac2f07b641
Fuentes-Elmer <delacruzelmer27@gmail.com>
2025-05-05 22:58:12 -0600

    Update capa conexiòn. Creaciòn de meto select usuario

--- Datos/cd_Usuarios.cs
+++ Datos/cd_Usuarios.cs
@@ -1,15 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Datos
+namespace capa_datos
 {
     public class cd_Usuarios
     {
         #region = "instancias de la clase conexion";
         private conexion connex = new conexion();
         #endregion
+
+        #region = "Metodo para vista del select o mostra en el dgv";
+        public DataTable MtdViewUsuarios()
+        {
+            string query = "select * from usuario";
+            SqlDataAdapter retornar = new SqlDataAdapter(query, connex.MtdAbrirconexion());
+            DataTable datosUsuarios = new DataTable();
+            retornar.Fill(datosUsuarios);
+            try //se opto por try para retornarlos datos 
+            {
+                return datosUsuarios;
+            }
+            finally //garantiza que se ejecute el cierre de la conexión
+            //esto por un error de accesibilidad que daba como alerta
+            {
+                connex.MtdCerrarconexion();
+            }
+
+        }
+        #endregion
+
+
     }
 }
--- Datos/conexion.cs
+++ Datos/conexion.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Datos
+namespace capa_datos
 {
     public class conexion
     {
+        // Objeto SqlConnection usado para establecer la conexión
+        private readonly SqlConnection conexiondatabase;
+
+        //inicializa la cadena de conexión
+        public conexion()
+        {
+            string cadenaConexion = "Server=tcp:sources.database.windows.net,1433;Initial Catalog=Proyecto_Restaurante;Persist Security Info=False;User ID=Administrador;Password=Sources01081997;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            conexiondatabase = new SqlConnection(cadenaConexion);
+        }
+
+        public SqlConnection MtdConexion { get; set; }
+
+        public SqlConnection MtdAbrirconexion()
+        {
+            if (conexiondatabase.State == ConnectionState.Closed)
+            {
+                conexiondatabase.Open();
+            }
+            return conexiondatabase;
+        }
+
+        public void MtdCerrarconexion()
+        {
+            if (conexiondatabase.State == ConnectionState.Open)
+            {
+                conexiondatabase.Close();
+            }
+        }
     }
 }
