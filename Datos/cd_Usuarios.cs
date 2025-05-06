4675b5783fa9e8bb1bee2e886b19cb295c44dde6
Fuentes-Elmer <delacruzelmer27@gmail.com>
2025-05-05 22:54:24 -0600

    instancias de la clase conexion en clase cd_usuarios

---
+++ Datos/cd_Usuarios.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class cd_Usuarios
+    {
+        #region = "instancias de la clase conexion";
+        private conexion connex = new conexion();
+        #endregion
+    }
+}
