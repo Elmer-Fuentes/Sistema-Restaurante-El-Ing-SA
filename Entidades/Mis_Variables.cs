b1f1102f63b0ce164bce738ecf383b8f0f1dc466
Fuentes-Elmer <delacruzelmer27@gmail.com>
2025-05-09 11:16:43 -0600

    New Capa Entidades

---
+++ Entidades/Mis_Variables.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public static class Mis_Variables
+    {
+            public static string UsuarioLogueado { get; set; }
+            public static string rolusuario { get; set; }
+    }
+}
