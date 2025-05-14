2997d58e55ff467aa323a255ad4031b2f2c3622f
EArévalo <elopeza58@miumg.edu.gt>
2025-05-13 19:51:32 -0600

    creación de query e inserción de datos en empleados, pago_planillas y encabezado_ordenes

---
+++ Soluciòn.SQL Sever 2022/Restaurantes El Inge S.A/7.1 Ins tbl_encabezado_ordenes.sql
@@ -0,0 +1,3 @@
+insert into tbl_encabezado_ordenes (codigo_cliente, codigo_mesa, codigo_empleado, fecha_orden, monto_total_orden, estado, usuario_sistema, fecha_sistema
+) values (1, 1, 1, '2024-05-10 12:30:00', 150.00, 'activa', 'admin01', '2024-05-10 12:31:00'),
+(5, 2, 2, '2024-05-10 13:15:00', 275.50, 'activa', 'admin02', '2024-05-10 13:16:00');
