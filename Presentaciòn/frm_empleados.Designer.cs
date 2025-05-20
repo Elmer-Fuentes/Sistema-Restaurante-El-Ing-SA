d130fd672b398435ea5297e9c664308340beade1
EArévalo <elopeza58@miumg.edu.gt>
2025-05-20 11:41:39 -0600

    Mejora de Diseño frm_empleados

--- Presentaciòn/frm_empleados.Designer.cs
+++ Presentaciòn/frm_empleados.Designer.cs
@@ -37,30 +37,33 @@
 			this.dgvEmpleados = new System.Windows.Forms.DataGridView();
 			this.groupBox1 = new System.Windows.Forms.GroupBox();
 			this.cboxcargo = new System.Windows.Forms.ComboBox();
-			this.dtp_contratacion = new System.Windows.Forms.DateTimePicker();
+			this.btnCancelar = new FontAwesome.Sharp.IconButton();
+			this.btnEditar = new FontAwesome.Sharp.IconButton();
+			this.btnGuardar = new FontAwesome.Sharp.IconButton();
 			this.label7 = new System.Windows.Forms.Label();
 			this.label6 = new System.Windows.Forms.Label();
 			this.label5 = new System.Windows.Forms.Label();
 			this.label4 = new System.Windows.Forms.Label();
 			this.label3 = new System.Windows.Forms.Label();
 			this.label2 = new System.Windows.Forms.Label();
-			this.label1 = new System.Windows.Forms.Label();
 			this.lblFecha = new System.Windows.Forms.Label();
 			this.label8 = new System.Windows.Forms.Label();
 			this.btnEliminar = new FontAwesome.Sharp.IconButton();
-			this.btnCancelar = new FontAwesome.Sharp.IconButton();
-			this.btnEditar = new FontAwesome.Sharp.IconButton();
-			this.btnGuardar = new FontAwesome.Sharp.IconButton();
 			this.btnSalir = new FontAwesome.Sharp.IconButton();
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.label11 = new System.Windows.Forms.Label();
+			this.DtpFechaPago = new System.Windows.Forms.DateTimePicker();
 			((System.ComponentModel.ISupportInitialize)(this.dgvEmpleados)).BeginInit();
 			this.groupBox1.SuspendLayout();
+			this.panel2.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// lbl_salario
 			// 
 			this.lbl_salario.AutoSize = true;
 			this.lbl_salario.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.lbl_salario.Location = new System.Drawing.Point(614, 18);
+			this.lbl_salario.Location = new System.Drawing.Point(631, 19);
 			this.lbl_salario.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
 			this.lbl_salario.Name = "lbl_salario";
 			this.lbl_salario.Size = new System.Drawing.Size(109, 17);
@@ -71,10 +74,10 @@
 			// 
 			this.cbox_estado.FormattingEnabled = true;
 			this.cbox_estado.Items.AddRange(new object[] {
-            "Pendiente ",
-            "Proceso ",
-            "Pagado ",
-            "Rechazado"});
+            "Activo",
+            "Inactivo",
+            "Suspendido",
+            "Despedido"});
 			this.cbox_estado.Location = new System.Drawing.Point(617, 87);
 			this.cbox_estado.Name = "cbox_estado";
 			this.cbox_estado.Size = new System.Drawing.Size(164, 21);
@@ -114,6 +117,7 @@
 			this.txt_codigo_empleado.Enabled = false;
 			this.txt_codigo_empleado.Location = new System.Drawing.Point(240, 16);
 			this.txt_codigo_empleado.Name = "txt_codigo_empleado";
+			this.txt_codigo_empleado.ReadOnly = true;
 			this.txt_codigo_empleado.Size = new System.Drawing.Size(165, 20);
 			this.txt_codigo_empleado.TabIndex = 14;
 			// 
@@ -121,20 +125,21 @@
 			// 
 			this.dgvEmpleados.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
 			this.dgvEmpleados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.dgvEmpleados.Location = new System.Drawing.Point(15, 241);
+			this.dgvEmpleados.Location = new System.Drawing.Point(15, 267);
 			this.dgvEmpleados.Margin = new System.Windows.Forms.Padding(2);
 			this.dgvEmpleados.Name = "dgvEmpleados";
 			this.dgvEmpleados.ReadOnly = true;
 			this.dgvEmpleados.RowHeadersWidth = 51;
 			this.dgvEmpleados.RowTemplate.Height = 24;
 			this.dgvEmpleados.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-			this.dgvEmpleados.Size = new System.Drawing.Size(1004, 228);
+			this.dgvEmpleados.Size = new System.Drawing.Size(1004, 202);
 			this.dgvEmpleados.TabIndex = 63;
 			// 
 			// groupBox1
 			// 
+			this.groupBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+			this.groupBox1.Controls.Add(this.DtpFechaPago);
 			this.groupBox1.Controls.Add(this.cboxcargo);
-			this.groupBox1.Controls.Add(this.dtp_contratacion);
 			this.groupBox1.Controls.Add(this.lbl_salario);
 			this.groupBox1.Controls.Add(this.cbox_estado);
 			this.groupBox1.Controls.Add(this.label10);
@@ -150,7 +155,7 @@
 			this.groupBox1.Controls.Add(this.label4);
 			this.groupBox1.Controls.Add(this.label3);
 			this.groupBox1.Controls.Add(this.label2);
-			this.groupBox1.Location = new System.Drawing.Point(15, 70);
+			this.groupBox1.Location = new System.Drawing.Point(19, 96);
 			this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
 			this.groupBox1.Name = "groupBox1";
 			this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
@@ -162,21 +167,60 @@
 			// 
 			this.cboxcargo.FormattingEnabled = true;
 			this.cboxcargo.Items.AddRange(new object[] {
-            "Pendiente ",
-            "Proceso ",
-            "Pagado ",
-            "Rechazado"});
+            "Gerente",
+            "Cocinero",
+            "Mesero",
+            "Cajero",
+            "Bodeguero"});
 			this.cboxcargo.Location = new System.Drawing.Point(240, 84);
 			this.cboxcargo.Name = "cboxcargo";
 			this.cboxcargo.Size = new System.Drawing.Size(164, 21);
 			this.cboxcargo.TabIndex = 62;
 			// 
-			// dtp_contratacion
+			// btnCancelar
 			// 
-			this.dtp_contratacion.Location = new System.Drawing.Point(615, 53);
-			this.dtp_contratacion.Name = "dtp_contratacion";
-			this.dtp_contratacion.Size = new System.Drawing.Size(160, 20);
-			this.dtp_contratacion.TabIndex = 61;
+			this.btnCancelar.IconChar = FontAwesome.Sharp.IconChar.Remove;
+			this.btnCancelar.IconColor = System.Drawing.Color.Black;
+			this.btnCancelar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+			this.btnCancelar.IconSize = 25;
+			this.btnCancelar.Location = new System.Drawing.Point(842, 84);
+			this.btnCancelar.Margin = new System.Windows.Forms.Padding(2);
+			this.btnCancelar.Name = "btnCancelar";
+			this.btnCancelar.Size = new System.Drawing.Size(129, 31);
+			this.btnCancelar.TabIndex = 22;
+			this.btnCancelar.Text = "Cancelar";
+			this.btnCancelar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+			this.btnCancelar.UseVisualStyleBackColor = true;
+			// 
+			// btnEditar
+			// 
+			this.btnEditar.IconChar = FontAwesome.Sharp.IconChar.SquarePen;
+			this.btnEditar.IconColor = System.Drawing.Color.Black;
+			this.btnEditar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+			this.btnEditar.IconSize = 25;
+			this.btnEditar.Location = new System.Drawing.Point(842, 47);
+			this.btnEditar.Margin = new System.Windows.Forms.Padding(2);
+			this.btnEditar.Name = "btnEditar";
+			this.btnEditar.Size = new System.Drawing.Size(129, 31);
+			this.btnEditar.TabIndex = 21;
+			this.btnEditar.Text = "Editar";
+			this.btnEditar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+			this.btnEditar.UseVisualStyleBackColor = true;
+			// 
+			// btnGuardar
+			// 
+			this.btnGuardar.IconChar = FontAwesome.Sharp.IconChar.UserPlus;
+			this.btnGuardar.IconColor = System.Drawing.Color.Black;
+			this.btnGuardar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+			this.btnGuardar.IconSize = 25;
+			this.btnGuardar.Location = new System.Drawing.Point(842, 12);
+			this.btnGuardar.Margin = new System.Windows.Forms.Padding(2);
+			this.btnGuardar.Name = "btnGuardar";
+			this.btnGuardar.Size = new System.Drawing.Size(129, 31);
+			this.btnGuardar.TabIndex = 20;
+			this.btnGuardar.Text = "Guardar";
+			this.btnGuardar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+			this.btnGuardar.UseVisualStyleBackColor = true;
 			// 
 			// label7
 			// 
@@ -242,22 +286,12 @@
 			this.label2.TabIndex = 1;
 			this.label2.Text = "Codigo Empleado:";
 			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.Location = new System.Drawing.Point(412, 23);
-			this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(131, 26);
-			this.label1.TabIndex = 59;
-			this.label1.Text = "Empleados";
-			// 
 			// lblFecha
 			// 
 			this.lblFecha.AutoSize = true;
+			this.lblFecha.BackColor = System.Drawing.SystemColors.ControlLightLight;
 			this.lblFecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.lblFecha.Location = new System.Drawing.Point(735, 51);
+			this.lblFecha.Location = new System.Drawing.Point(730, 77);
 			this.lblFecha.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
 			this.lblFecha.Name = "lblFecha";
 			this.lblFecha.Size = new System.Drawing.Size(100, 17);
@@ -267,8 +301,9 @@
 			// label8
 			// 
 			this.label8.AutoSize = true;
+			this.label8.BackColor = System.Drawing.SystemColors.ControlLightLight;
 			this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label8.Location = new System.Drawing.Point(629, 51);
+			this.label8.Location = new System.Drawing.Point(633, 77);
 			this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
 			this.label8.Name = "label8";
 			this.label8.Size = new System.Drawing.Size(93, 17);
@@ -290,51 +325,6 @@
 			this.btnEliminar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
 			this.btnEliminar.UseVisualStyleBackColor = true;
 			// 
-			// btnCancelar
-			// 
-			this.btnCancelar.IconChar = FontAwesome.Sharp.IconChar.Remove;
-			this.btnCancelar.IconColor = System.Drawing.Color.Black;
-			this.btnCancelar.IconFont = FontAwesome.Sharp.IconFont.Auto;
-			this.btnCancelar.IconSize = 25;
-			this.btnCancelar.Location = new System.Drawing.Point(842, 84);
-			this.btnCancelar.Margin = new System.Windows.Forms.Padding(2);
-			this.btnCancelar.Name = "btnCancelar";
-			this.btnCancelar.Size = new System.Drawing.Size(129, 31);
-			this.btnCancelar.TabIndex = 22;
-			this.btnCancelar.Text = "Cancelar";
-			this.btnCancelar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-			this.btnCancelar.UseVisualStyleBackColor = true;
-			// 
-			// btnEditar
-			// 
-			this.btnEditar.IconChar = FontAwesome.Sharp.IconChar.SquarePen;
-			this.btnEditar.IconColor = System.Drawing.Color.Black;
-			this.btnEditar.IconFont = FontAwesome.Sharp.IconFont.Auto;
-			this.btnEditar.IconSize = 25;
-			this.btnEditar.Location = new System.Drawing.Point(842, 47);
-			this.btnEditar.Margin = new System.Windows.Forms.Padding(2);
-			this.btnEditar.Name = "btnEditar";
-			this.btnEditar.Size = new System.Drawing.Size(129, 31);
-			this.btnEditar.TabIndex = 21;
-			this.btnEditar.Text = "Editar";
-			this.btnEditar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-			this.btnEditar.UseVisualStyleBackColor = true;
-			// 
-			// btnGuardar
-			// 
-			this.btnGuardar.IconChar = FontAwesome.Sharp.IconChar.UserPlus;
-			this.btnGuardar.IconColor = System.Drawing.Color.Black;
-			this.btnGuardar.IconFont = FontAwesome.Sharp.IconFont.Auto;
-			this.btnGuardar.IconSize = 25;
-			this.btnGuardar.Location = new System.Drawing.Point(842, 12);
-			this.btnGuardar.Margin = new System.Windows.Forms.Padding(2);
-			this.btnGuardar.Name = "btnGuardar";
-			this.btnGuardar.Size = new System.Drawing.Size(129, 31);
-			this.btnGuardar.TabIndex = 20;
-			this.btnGuardar.Text = "Guardar";
-			this.btnGuardar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-			this.btnGuardar.UseVisualStyleBackColor = true;
-			// 
 			// btnSalir
 			// 
 			this.btnSalir.IconChar = FontAwesome.Sharp.IconChar.PowerOff;
@@ -350,18 +340,57 @@
 			this.btnSalir.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
 			this.btnSalir.UseVisualStyleBackColor = true;
 			// 
+			// tabControl1
+			// 
+			this.tabControl1.Location = new System.Drawing.Point(-3, 2);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(1041, 550);
+			this.tabControl1.TabIndex = 66;
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.Color.DarkOrange;
+			this.panel2.Controls.Add(this.label11);
+			this.panel2.Location = new System.Drawing.Point(-3, 2);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(1041, 62);
+			this.panel2.TabIndex = 85;
+			// 
+			// label11
+			// 
+			this.label11.AutoSize = true;
+			this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label11.Location = new System.Drawing.Point(491, 18);
+			this.label11.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.label11.Name = "label11";
+			this.label11.Size = new System.Drawing.Size(131, 26);
+			this.label11.TabIndex = 64;
+			this.label11.Text = "Empleados";
+			// 
+			// DtpFechaPago
+			// 
+			this.DtpFechaPago.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.DtpFechaPago.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+			this.DtpFechaPago.Location = new System.Drawing.Point(616, 52);
+			this.DtpFechaPago.Margin = new System.Windows.Forms.Padding(2);
+			this.DtpFechaPago.Name = "DtpFechaPago";
+			this.DtpFechaPago.Size = new System.Drawing.Size(165, 21);
+			this.DtpFechaPago.TabIndex = 63;
+			// 
 			// frm_empleados
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(1034, 549);
+			this.Controls.Add(this.panel2);
 			this.Controls.Add(this.btnEliminar);
 			this.Controls.Add(this.dgvEmpleados);
 			this.Controls.Add(this.groupBox1);
 			this.Controls.Add(this.btnSalir);
-			this.Controls.Add(this.label1);
 			this.Controls.Add(this.lblFecha);
 			this.Controls.Add(this.label8);
+			this.Controls.Add(this.tabControl1);
 			this.Name = "frm_empleados";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "frm_empleados";
@@ -369,6 +398,8 @@
 			((System.ComponentModel.ISupportInitialize)(this.dgvEmpleados)).EndInit();
 			this.groupBox1.ResumeLayout(false);
 			this.groupBox1.PerformLayout();
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -395,10 +426,12 @@
 		private System.Windows.Forms.Label label3;
 		private System.Windows.Forms.Label label2;
 		private FontAwesome.Sharp.IconButton btnSalir;
-		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Label lblFecha;
 		private System.Windows.Forms.Label label8;
 		private System.Windows.Forms.ComboBox cboxcargo;
-		private System.Windows.Forms.DateTimePicker dtp_contratacion;
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Label label11;
+		private System.Windows.Forms.DateTimePicker DtpFechaPago;
 	}
 }
